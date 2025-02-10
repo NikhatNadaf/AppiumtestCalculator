560ed77518f52a88c49283daebdf73600e1da9ef
Nikhat Nadaf <nikhatnadaf1128@gmail.com>
2025-02-10 23:54:24 +0530

    Appium

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,23 +1,16 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 import java.net.MalformedURLException;
 import java.net.URL;
- 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
- 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
- 
 public class CalculatorTest {
  
     private AppiumDriver driver;
@@ -26,7 +19,7 @@ public class CalculatorTest {
     public void setUp() throws MalformedURLException {
     	 DesiredCapabilities caps = new DesiredCapabilities();
          caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "motorola moto g85 5G");
+         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "realme RMX3430");
          caps.setCapability("appPackage", "com.example.calculatorapp");
          caps.setCapability("appActivity", "com.example.calculatorapp.MainActivity");
          caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
@@ -41,23 +34,20 @@ public class CalculatorTest {
    	 WebElement num1Field = driver.findElement(By.id("com.example.calculatorapp:id/etFirstNumber"));
    	 System.out.println("NumField 1 : "+num1Field);
          num1Field.sendKeys("5");
- 
          WebElement num2Field = driver.findElement(By.id("com.example.calculatorapp:id/etSecondNumber"));
          num2Field.sendKeys("3");
- 
          WebElement addButton = driver.findElement(By.id("com.example.calculatorapp:id/btnAdd"));
        addButton.click();
- 
-       WebElement resultView = driver.findElement(By.id("com.example.calculatorapp:id/textViewMessage"));
+       WebElement resultView = driver.findElement(By.id("com.example.calculatorapp:id/tvResult"));
         // Assert the result
         String resultText = resultView.getText();
-        assertEquals("5", resultText);
+        System.out.println(resultText);
+        assertEquals("Result: 8.0", resultText);
     }
- 
     @After
     public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
     }
-}}
+}
